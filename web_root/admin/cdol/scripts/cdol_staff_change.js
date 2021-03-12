b63070b68690f1cda6af1e7ccd2f895c74a6e5a4
adriancarlson <adrian.r.carlson@gmail.com>
2021-03-12 14:46:56 -0600

    21.3.0.4 - angularize form

---
+++ web_root/admin/cdol/scripts/cdol_staff_change.js
@@ -0,0 +1,8 @@
+require(['angular', 'components/shared/index'], function (angular) {
+	var myApp = angular.module('cdolStaffApp', ['powerSchoolModule']);
+	myApp.controller('cdolStaffAppCtrl', function ($scope, $http) {
+		$scope.newStaffFirstName = 'New Staff';
+        $scope.newStaffLastName = '';
+	});
+	angular.bootstrap($j('#cdolStaffAppDiv'), ['cdolStaffApp']);
+});
