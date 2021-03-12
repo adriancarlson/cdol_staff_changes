9913d38a3dd37c32a6f6977fb1c7247cd2deedae
adriancarlson <adrian.r.carlson@gmail.com>
2021-03-12 14:54:43 -0600

    21.3.0.5 - attempt to fix angular load

--- web_root/admin/cdol/scripts/cdol_staff_change.js
+++ web_root/admin/cdol/scripts/cdol_staff_change.js
@@ -2,7 +2,7 @@ require(['angular', 'components/shared/index'], function (angular) {
 	var myApp = angular.module('cdolStaffApp', ['powerSchoolModule']);
 	myApp.controller('cdolStaffAppCtrl', function ($scope, $http) {
 		$scope.newStaffFirstName = 'New Staff';
-        $scope.newStaffLastName = '';
+		$scope.newStaffLastName = '';
 	});
 	angular.bootstrap($j('#cdolStaffAppDiv'), ['cdolStaffApp']);
 });
