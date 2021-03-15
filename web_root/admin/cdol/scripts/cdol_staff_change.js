e0c2ee31ffec3fa8945532a0291f181018ca4dee
adriancarlson <adrian.r.carlson@gmail.com>
2021-03-15 14:45:59 -0500

    21.3.0.19 - min proofing angular

--- web_root/admin/cdol/scripts/cdol_staff_change.js
+++ web_root/admin/cdol/scripts/cdol_staff_change.js
@@ -1,14 +1,19 @@
 require(['angular', 'components/shared/index'], function (angular) {
 	var myApp = angular.module('cdolStaffApp', ['powerSchoolModule']);
-	myApp.controller('cdolStaffAppCtrl', function ($scope, $http) {
-		$scope.newStaffTempName = 'the New Staff Member';
-		$scope.newStaffTitle = '';
-		$scope.newStaffFirstName = '';
-		$scope.newStaffLastName = '';
+	myApp.controller('cdolStaffAppCtrl', [
+		'$scope',
+		'$http',
+		'$attrs',
+		function ($scope, $http, $attrs) {
+			$scope.newStaffTempName = 'the New Staff Member';
+			$scope.newStaffTitle = '';
+			$scope.newStaffFirstName = '';
+			$scope.newStaffLastName = '';
 
-		$scope.getStaffResults = function () {
-			console.log('getStaffResults ran');
-		};
-	});
+			$scope.getStaffResults = function () {
+				console.log('getStaffResults ran');
+			};
+		},
+	]);
 	angular.bootstrap($j('#cdolStaffAppDiv'), ['cdolStaffApp']);
 });
