a87dd36f79761755b40eb101963c1c3f52b6cdd0
adriancarlson <adrian.r.carlson@gmail.com>
2021-03-16 16:13:15 -0500

    21.3.0.39 - complete restructure to be more SPA

--- web_root/admin/cdol/staffchange/scripts/cdol_staff_change.js
+++ web_root/admin/cdol/staffchange/scripts/cdol_staff_change.js
@@ -11,11 +11,38 @@ require(['angular', 'components/shared/index'], function (angular) {
 			$scope.newStaffLastName = '';
 			$scope.curSchoolId = $attrs.ngCurSchoolId;
 			$scope.curYearId = $attrs.ngCurYearId;
+			$scope.pageStatus = 'submit';
 
+			// form directive
+			myApp.directive('changeDirective', function () {
+				return {
+					restrict: 'E',
+					templateUrl: '/admin/cdol/staffchange/templates/cdol_staff_change_form.html',
+					replace: true,
+				};
+			});
+			//confirm directive
+			myApp.directive('confirmDirective', function () {
+				return {
+					restrict: 'E',
+					templateUrl: '/admin/cdol/staffchange/templates/cdol_staff_change_confirm.html',
+					replace: true,
+				};
+			});
+			//edit directive
+			myApp.directive('editDirective', function () {
+				return {
+					restrict: 'E',
+					templateUrl: '/admin/cdol/staffchange/templates/cdol_staff_change_edit.html',
+					replace: true,
+				};
+			});
+
+			// list page controller
 			$scope.getStaffResults = function () {
 				loadingDialog();
 				$http({
-					url: '/admin/cdol/scripts/getStaffChanges.json',
+					url: '/admin/cdol/staffchange/data/getStaffChanges.json',
 					method: 'GET',
 					params: { curSchoolId: $scope.curSchoolId, curYearId: $scope.curYearId },
 				}).then(
