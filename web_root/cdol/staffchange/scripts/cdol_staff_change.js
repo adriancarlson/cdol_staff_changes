bdf007e14739281bf360d38ba40de97a590757a9
adriancarlson <adrian.r.carlson@gmail.com>
2021-03-17 11:45:52 -0500

    21.3.0.52 - begin new submission step frame work

--- web_root/cdol/staffchange/scripts/cdol_staff_change.js
+++ web_root/cdol/staffchange/scripts/cdol_staff_change.js
@@ -6,25 +6,71 @@ define(['angular', 'components/shared/index'], function (angular) {
 		'$attrs',
 		function ($scope, $http, $attrs) {
 			$scope.newStaffTempName = 'the New Staff Member';
-			$scope.newStaffTitle = '';
-			$scope.newStaffFirstName = '';
-			$scope.newStaffLastName = '';
-			$scope.curSchoolId = $attrs.ngCurSchoolId;
-			$scope.curYearId = $attrs.ngCurYearId;
+			// $scope.newStaffTitle = '';
+			// $scope.newStaffFirstName = '';
+			// $scope.newStaffLastName = '';
+			// $scope.curSchoolId = $attrs.ngCurSchoolId;
+			// $scope.curYearId = $attrs.ngCurYearId;
+			// $scope.curUserID = $attrs.ngCurUserID;
+			// $scope.curDate = $attrs.ngCurDateID;
 			$scope.pageStatus = $attrs.ngStatus;
 
-			// list page controller
+			$scope.newStaff = {
+				iid: '-1',
+				schoolid: $attrs.ngCurSchoolId,
+				yearid: $attrs.ngCurYearId,
+				title: '',
+				first_name: '',
+				middle_name: '',
+				last_name: '',
+				preferred_name: '',
+				maiden_name: '',
+				gender: '',
+				personal_email: '',
+				staff_type: '',
+				position: '',
+				replacing: '',
+				start_date: '',
+				previous: '',
+				previous_employer: '',
+				previous_employer_other: '',
+				submission_date: $attrs.ngCurDateID,
+				who_submitted: $attrs.ngCurUserID,
+				ps_created: '',
+				ad_created: '',
+			};
+
+			// ajax call to list records in staff U_CDOL_STAFF_CHANGES table
 			$scope.getStaffResults = function () {
 				loadingDialog();
 				$http({
 					url: '/admin/cdol/staffchange/data/getStaffChanges.json',
 					method: 'GET',
-					params: { curSchoolId: $scope.curSchoolId, curYearId: $scope.curYearId },
+					params: { curSchoolId: $attrs.ngCurSchoolId, curYearId: $attrs.ngCurYearId },
 				}).then(
 					function successCallback(response) {
 						$scope.staffList = response.data;
 						$scope.staffList.pop();
 						closeLoading();
+					},
+					function errorCallback(response) {
+						alert('Error Loading Data');
+						closeLoading();
+					},
+				);
+			};
+
+			// submitting new staff change record
+			$scope.submitStaffChange = function () {
+				loadingDialog();
+				$http({
+					url: '/admin/cdol/staffchange/data/staffchangerec.html',
+					method: 'POST',
+					data: $scope.newStaff,
+				}).then(
+					function successCallback(response) {
+						closeLoading();
+						$scope.pageStatus = 'Confirm';
 					},
 					function errorCallback(response) {
 						alert('Error Loading Data');
