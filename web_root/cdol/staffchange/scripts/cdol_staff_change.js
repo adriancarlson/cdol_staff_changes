00b24336a7849bb7479c2deef5b39b475c12bc26
adriancarlson <adrian.r.carlson@gmail.com>
2021-03-24 09:56:26 -0500

    21.3.0.72 - gitting rid of directives

--- web_root/cdol/staffchange/scripts/cdol_staff_change.js
+++ web_root/cdol/staffchange/scripts/cdol_staff_change.js
@@ -6,19 +6,18 @@ define(['angular', 'components/shared/index'], function (angular) {
 		'$attrs',
 		'$q',
 		function ($scope, $http, $attrs, $q) {
-			$scope.newStaffTempName = 'the New Staff Member';
-			// $scope.newStaffTitle = '';
-			// $scope.newStaffFirstName = '';
-			// $scope.newStaffLastName = '';
-			// $scope.curSchoolId = $attrs.ngCurSchoolId;
-			// $scope.curYearId = $attrs.ngCurYearId;
-			// $scope.curUserID = $attrs.ngCurUserID;
-			// $scope.curDate = $attrs.ngCurDateID;
-			$scope.pageStatus = $attrs.ngStatus;
+			$scope.userContext = {
+				newStaffTempName: 'the New Staff Member',
+				pageStatus: $attrs.ngStatus,
+				curSchoolId: $attrs.ngCurSchoolId,
+				curYearId: $attrs.ngCurYearId,
+				curDate: $attrs.ngCurDate,
+				curUserId: $attrs.ngCurUserId,
+			};
 
 			$scope.newStaff = {
-				schoolid: $attrs.ngCurSchoolId,
-				yearid: $attrs.ngCurYearId,
+				schoolid: $scope.userContext.curSchoolId,
+				yearid: $scope.userContext.curYearId,
 				title: '',
 				first_name: '',
 				middle_name: '',
@@ -34,29 +33,10 @@ define(['angular', 'components/shared/index'], function (angular) {
 				start_date: '',
 				previous_employer: '',
 				previous_employer_other: '',
-				submission_date: $attrs.ngCurDateID,
-				who_submitted: $attrs.ngCurUserID,
+				submission_date: $scope.userContext.curDate,
+				who_submitted: $scope.userContext.curUserId,
 				ps_created: '',
 				ad_created: '',
-			};
-			//@param {string} dt (mm/dd/yyyy)
-			//@return {string} (yyyy-mm-dd)
-			$scope.formatDateForApi = function (dt) {
-				var dateParts = dt.split('/');
-				var m = dateParts[0];
-				var d = dateParts[1];
-				var y = dateParts[2];
-				return y + '-' + m + '-' + d;
-			};
-
-			//@param {string} dt (yyyy-dd-mm)
-			//@return {string} (mm/dd/yyyy)
-			$scope.formatDateFromApi = function (dt) {
-				var dateParts = dt.split('-');
-				var y = dateParts[0];
-				var m = dateParts[1];
-				var d = dateParts[2];
-				return m + '/' + d + '/' + y;
 			};
 
 			// function to get PQ results
@@ -91,92 +71,8 @@ define(['angular', 'components/shared/index'], function (angular) {
 				});
 				closeLoading();
 			};
-
-			$scope.sendEmail = function () {
-				console.log('sendEmail Function Ran');
-			};
-
-			// submitting new staff change record
-
-			$scope.submitStaffChange = function () {
-				loadingDialog();
-
-				var newRecord = {
-					tables: {
-						U_CDOL_STAFF_CHANGES: {
-							schoolid: $scope.newStaff.schoolid,
-							yearid: $scope.newStaff.yearid,
-							title: $scope.newStaff.title,
-							first_name: $scope.newStaff.first_name,
-							middle_name: $scope.newStaff.middle_name,
-							last_name: $scope.newStaff.last_name,
-							preferred_name: $scope.newStaff.preferred_name,
-							maiden_name: $scope.newStaff.maiden_name,
-							gender: $scope.newStaff.gender,
-							personal_email: $scope.newStaff.personal_email,
-							staff_type: $scope.newStaff.staff_type,
-							position: $scope.newStaff.position,
-							replacing: $scope.newStaff.replacing,
-							start_date: $scope.formatDateForApi($scope.newStaff.start_date),
-							previous: $scope.newStaff.previous,
-							previous_employer: $scope.newStaff.previous_employer,
-							previous_employer_other: $scope.newStaff.previous_employer_other,
-							submission_date: $scope.formatDateForApi($scope.newStaff.submission_date),
-							who_submitted: $scope.newStaff.who_submitted,
-							ps_created: $scope.newStaff.ps_created,
-							ad_created: $scope.newStaff.ad_created,
-						},
-					},
-				};
-
-				$http({
-					url: '/ws/schema/table/U_CDOL_STAFF_CHANGES',
-					method: 'POST',
-					data: newRecord,
-					headers: {
-						Accept: 'application/json',
-						'Content-Type': 'application/json',
-					},
-				}).then(function (response) {
-					if (response.data.result[0].status == 'SUCCESS') {
-						console.log(response);
-						closeLoading();
-						$scope.pageStatus = 'Confirm';
-					} else {
-						psAlert({ message: 'There was an error submitting the record. Please contact The Ed Tech office', title: 'Staff Change Submission Error' });
-						closeLoading();
-					}
-				});
-			};
 		},
 	]);
-
-	//form directive
-	cdolStaffApp.directive('formDirective', function () {
-		return {
-			restrict: 'E',
-			templateUrl: '/admin/cdol/staffchange/templates/form.html',
-			// replace: true,
-			controller: ('formController', ['$scope', '$http', '$attrs', '$q', function ($scope, $http, $attrs, $q) {}]),
-			controllerAs: 'form',
-		};
-	});
-	//edit directive
-	cdolStaffApp.directive('editDirective', function () {
-		return {
-			restrict: 'E',
-			templateUrl: '/admin/cdol/staffchange/templates/edit.html',
-			// replace: true,
-		};
-	});
-	//confirm directive
-	cdolStaffApp.directive('confirmDirective', function () {
-		return {
-			restrict: 'E',
-			templateUrl: '/admin/cdol/staffchange/templates/confirm.html',
-			// replace: true,
-		};
-	});
 
 	angular.bootstrap($j('#cdolStaffAppDiv'), ['cdolStaffApp']);
 });
