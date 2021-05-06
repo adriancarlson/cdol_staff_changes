54cfff9d6a8bb0fd2a41d1ec85d15d29f73f8df2
adriancarlson <adrian.r.carlson@gmail.com>
2021-05-06 09:30:58 -0500

    21.5.0.20 - exiting email on submit

--- web_root/cdol/staffchange/scripts/cdol_staff_change.js
+++ web_root/cdol/staffchange/scripts/cdol_staff_change.js
@@ -241,6 +241,40 @@ define(['angular', 'components/shared/index', '/mbaReportCreator/scripts/dateSer
 						},
 					}).then(function (response) {
 						if (response.data.result[0].status == 'SUCCESS') {
+							//email for exitingstaff record.
+							$j.ajax({
+								url: '/admin/cdol/staffchange/data/emailfields.html',
+								method: 'POST',
+								contentType: 'application/x-www-form-urlencoded',
+								data: {
+									curDate: $scope.userContext.CurDate,
+									curTime: $scope.userContext.CurTime,
+									emailFrom: $scope.userContext.curUserEmail,
+									emailTo: 'ps-support@cdolinc.net',
+									emailSubject: 'Exiting Staff Submission from ' + $scope.userContext.curUserName + ' (' + $scope.userContext.curUserSchoolAbbr + ') | ' + $scope.userContext.curUserEmail,
+									emailBody: 'Exiting Staff Name: ' + $scope.exitingRecord.first_name + ' ' + $scope.exitingRecord.last_name,
+								},
+								success: function (result) {
+									$j('#exitHoldingDiv').append(result);
+								},
+								complete: function () {
+									var data = {
+										ac: 'prim',
+									};
+									$j('#exitHoldingDiv')
+										.find('.fireaway')
+										.each(function () {
+											data[$j(this).attr('name')] = $j(this).val();
+										});
+									$j.ajax({
+										method: 'POST',
+										data: data,
+										complete: function () {
+											$j('#exitHoldingDiv').html('');
+										},
+									});
+								},
+							});
 						} else {
 							psAlert({ message: 'There was an error submitting the exit record. Changes were not saved', title: 'Error Submitting Record' });
 						}
