aab5a720a6aee981f8af6641bb24d48dcecd5ae4
adriancarlson <adrian.r.carlson@gmail.com>
2022-10-03 15:12:01 -0500

    22.10.3 - hooking up list controller

--- web_root/scripts/cdol_staff_change/cdol_staff_change_list.js
+++ web_root/scripts/cdol_staff_change/cdol_staff_change_list.js
@@ -1,5 +1,6 @@
-require(['angular', 'components/shared/index'], function (angular) {
+define(['angular', 'components/shared/index'], function (angular) {
 	var cdolStaffListApp = angular.module('cdolStaffListMod', ['powerSchoolModule']);
+
 	cdolStaffListApp.controller('cdolStaffListCtrl', function ($scope, $http, $attrs, $q) {
 		$scope.spinnerActive = true;
 		$scope.staffChangeCount = 0;
@@ -8,12 +9,36 @@ require(['angular', 'components/shared/index'], function (angular) {
 		$scope.jobChangeList = [];
 		$scope.nameChangeList = [];
 		$scope.exitStaffList = [];
-
+		$scope.curSchoolId = $attrs.ngCurSchoolId;
 		$scope.curYearId = $attrs.ngCurYearId;
 		$scope.curDate = $attrs.ngCurDate;
 		$scope.adjustedYearId = new Date($attrs.ngCurDate).getFullYear() - 1991;
 
-		$scope.loadingData = () => {
+		// function to get PQ results
+		$scope.getPowerQueryResults = function (endpoint, data) {
+			var deferredResponse = $q.defer();
+			$http({
+				url: '/ws/schema/query/' + endpoint,
+				method: 'POST',
+				data: data || {},
+				params: { pagesize: 0 },
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+			}).then(
+				function successCallback(response) {
+					deferredResponse.resolve(response.data.record || []);
+				},
+				function errorCallback(response) {
+					psAlert({ message: 'There was an error loading the Staff Change Data', title: 'Error Loading Data' });
+				},
+			);
+
+			return deferredResponse.promise;
+		};
+
+		$scope.loadData = function () {
 			loadingDialog();
 			$j.ajax({
 				url: '/admin/cdol/staffchange/data/newstaffcount.txt',
@@ -22,12 +47,9 @@ require(['angular', 'components/shared/index'], function (angular) {
 				},
 			});
 
-			$scope.getPowerQueryResults('net.cdolinc.staffChanges.staff.counts', { curSchoolID: $scope.curYearId, curYearID: $scope.adjustedYearId }).then((staffCountData) => {
+			$scope.getPowerQueryResults('net.cdolinc.staffChanges.staff.counts', { curSchoolID: $scope.curSchoolId, curYearID: $scope.adjustedYearId }).then((staffCountData) => {
 				$scope.staffChangeCount = staffCountData[0];
 			});
-
-			alert($scope.staffChangeCount);
-
 			closeLoading();
 		};
 
