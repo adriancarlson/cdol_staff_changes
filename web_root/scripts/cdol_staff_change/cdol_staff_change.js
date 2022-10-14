4699143f0b65547c331884af34647ac3ec165cca
adriancarlson <adrian.r.carlson@gmail.com>
2022-10-14 15:36:52 -0500

    22.10.56 - setting up getExisting staff func

--- web_root/scripts/cdol_staff_change/cdol_staff_change.js
+++ web_root/scripts/cdol_staff_change/cdol_staff_change.js
@@ -1,34 +1,31 @@
-define(['angular', 'components/shared/index', '/scripts/cdol/services/dateService.js', '/scripts/cdol/services/checkboxService.js'], function (angular) {
-	var cdolStaffApp = angular.module('cdolStaffAppMod', ['powerSchoolModule', 'dateService', 'checkboxModule']);
-	cdolStaffApp.controller('cdolStaffAppCtrl', [
-		'$scope',
-		'$http',
-		'$attrs',
-		'$q',
-		'$window',
-		'dateService',
-		'checkboxService',
-		function ($scope, $http, $attrs, $q, $window, dateService, checkboxService) {
-			$scope.userContext = {
-				pageStatus: $attrs.ngStatus,
-				curSchoolId: $attrs.ngCurSchoolId,
-				curYearId: $attrs.ngCurYearId,
-				curDate: $attrs.ngCurDate,
-				curTime: $attrs.ngCurTime,
-				staffChangeId: $attrs.ngStaffChangeId,
-				curUserId: $attrs.ngCurUserId,
-				curStaffId: $attrs.ngCurUserId,
-				curUserName: $attrs.ngCurUserName,
-				curUserEmail: $attrs.ngCurUserEmail,
-				curUserSchoolAbbr: $attrs.ngCurUserSchoolAbbr,
-				step: 'start',
-			};
+define(['angular', 'components/shared/index', '/scripts/cdol/services/dateService.js', '/scripts/cdol/services/checkboxService.js', 'scripts/cdol/services/camelService.js'], function (angular) {
+	var cdolStaffApp = angular.module('cdolStaffAppMod', ['powerSchoolModule', 'dateService', 'checkboxModule', 'camelModule']);
+	cdolStaffApp.controller('cdolStaffAppCtrl', function ($scope, $http, $attrs, $q, $window, dateService, checkboxService, camelService) {
+		$scope.userContext = {
+			pageStatus: $attrs.ngStatus,
+			curSchoolId: $attrs.ngCurSchoolId,
+			curYearId: $attrs.ngCurYearId,
+			curDate: $attrs.ngCurDate,
+			curTime: $attrs.ngCurTime,
+			staffChangeId: $attrs.ngStaffChangeId,
+			curUserId: $attrs.ngCurUserId,
+			curStaffId: $attrs.ngCurUserId,
+			curUserName: $attrs.ngCurUserName,
+			curUserEmail: $attrs.ngCurUserEmail,
+			curUserSchoolAbbr: $attrs.ngCurUserSchoolAbbr,
+			pageContext: 'start',
+		};
 
-			$scope.formDipslay = (step) => {
-				$scope.userContext.step = step;
-			};
-		},
-	]);
+		$scope.formDipslay = (pageContext) => {
+			$scope.userContext.pageContext = pageContext;
+		};
+
+		$scope.getExistingStaff = (pageContext, userDcid) => {
+			const camelPageContext = camelService.camelize(pageContext);
+			console.log(camelPageContext);
+			console.log(userDcid);
+		};
+	});
 	cdolStaffApp.directive('start', () => ({ templateUrl: '/admin/cdol/staff_change/directives/forms/start.html' }));
 	cdolStaffApp.directive('newStaff', () => ({ templateUrl: '/admin/cdol/staff_change/directives/forms/new_staff.html' }));
 	cdolStaffApp.directive('transferStaff', () => ({ templateUrl: '/admin/cdol/staff_change/directives/forms/transfer_staff.html' }));
