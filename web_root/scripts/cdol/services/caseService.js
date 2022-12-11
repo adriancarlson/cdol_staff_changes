25accb4662f2a7516d12fe8e35f36d43fc35a7bc
adriancarlson <adrian.r.carlson@gmail.com>
2022-12-11 12:43:27 -0600

    22.12.0 - new sentance case and updated versioning

--- web_root/scripts/cdol/services/caseService.js
+++ web_root/scripts/cdol/services/caseService.js
@@ -19,11 +19,18 @@ define(['angular'], function (angular) {
 		}
 
 		this.sentenceCase = function (str) {
-			return clean(str)
-				.replace(/[a-z]/i, function (letter) {
-					return letter.toUpperCase()
-				})
-				.trim()
+			var n = str.split('.')
+			var vfinal = ''
+			for (i = 0; i < n.length; i++) {
+				var spaceput = ''
+				var spaceCount = n[i].replace(/^(\s*).*$/, '$1').length
+				n[i] = n[i].replace(/^\s+/, '')
+				var newstring = n[i].charAt(n[i]).toUpperCase() + n[i].slice(1)
+				for (j = 0; j < spaceCount; j++) spaceput = spaceput + ' '
+				vfinal = vfinal + spaceput + newstring + '.'
+			}
+			vfinal = vfinal.substring(0, vfinal.length - 1)
+			return vfinal
 		}
 
 		this.titleCase = function (str) {
