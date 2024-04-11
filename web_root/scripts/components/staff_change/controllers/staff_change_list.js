a64f7c004f39705c8fcba00ac911b97522711a35
Adrian Carlson <adrian.r.carlson@gmail.com>
2024-04-11 15:38:18 -0500

    Filtering for new staff list

--- web_root/scripts/components/staff_change/controllers/staff_change_list.js
+++ web_root/scripts/components/staff_change/controllers/staff_change_list.js
@@ -7,7 +7,8 @@ define(function (require) {
 		'$scope',
 		'$attrs',
 		'pqService',
-		function ($scope, $attrs, pqService) {
+		'formatService',
+		function ($scope, $attrs, pqService, formatService) {
 			$scope.staffChangeCounts = []
 			$scope.staffList = {}
 			$scope.curSchoolId = $attrs.ngCurSchoolId
@@ -16,6 +17,48 @@ define(function (require) {
 			$scope.calendarYear = new Date().getFullYear()
 			$scope.selectedTab = document.querySelector('[aria-selected="true"]').getAttribute('data-context')
 			$scope.booleanMap = { Yes: true, No: false }
+			$scope.titleMap = {
+				'Mr.': 'Mr.',
+				'Mrs.': 'Mrs.',
+				'Ms.': 'Ms.',
+				'Dr.': 'Dr.',
+				'Fr.': 'Fr.',
+				'Msgr.': 'Msgr.',
+				'Sr.': 'Sr.',
+				'Br.': 'Br.'
+			}
+			$scope.schoolMap = {
+				'All Saints Catholic School Holdrege': 'All Saints Catholic School Holdrege',
+				'Aquinas Catholic Elementary': 'Aquinas Catholic Elementary',
+				'Aquinas Catholic Middle/High': 'Aquinas Catholic Middle/High',
+				'Bishop Neumann Catholic Jr/Sr High School': 'Bishop Neumann Catholic Jr/Sr High School',
+				'Blessed Sacrament School': 'Blessed Sacrament School',
+				'Cathedral of the Risen Christ School': 'Cathedral of the Risen Christ School',
+				'Falls City Sacred Heart Elementary': 'Falls City Sacred Heart Elementary',
+				'Falls City Sacred Heart Jr/Sr High School': 'Falls City Sacred Heart Jr/Sr High School',
+				'Lourdes Central Catholic Elementary School': 'Lourdes Central Catholic Elementary School',
+				'Lourdes Central Catholic Middle/High School': 'Lourdes Central Catholic Middle/High School',
+				'North American Martyrs School': 'North American Martyrs School',
+				'Pius X High School': 'Pius X High School',
+				'St. Andrew Tecumseh': 'St. Andrew Tecumseh',
+				'St. Cecilia Middle & High School': 'St. Cecilia Middle & High School',
+				'St. James Crete': 'St. James Crete',
+				'St. John Lincoln': 'St. John Lincoln',
+				'St. John Nepomucene Weston': 'St. John Nepomucene Weston',
+				'St. John the Baptist School': 'St. John the Baptist School',
+				'St. Joseph Beatrice': 'St. Joseph Beatrice',
+				'St. Joseph Lincoln': 'St. Joseph Lincoln',
+				'St. Joseph York': 'St. Joseph York',
+				'St. Michael Hastings': 'St. Michael Hastings',
+				'St. Michael Lincoln': 'St. Michael Lincoln',
+				'St. Patrick Lincoln': 'St. Patrick Lincoln',
+				'St. Patrick McCook': 'St. Patrick McCook',
+				'St. Peter Catholic School': 'St. Peter Catholic School',
+				'St. Teresa Elementary School': 'St. Teresa Elementary School',
+				'St. Vincent de Paul Seward': 'St. Vincent de Paul Seward',
+				'St. Wenceslaus Wahoo': 'St. Wenceslaus Wahoo',
+				'Villa Marie School': 'Villa Marie School'
+			}
 
 			$scope.loadData = async changeType => {
 				loadingDialog()
@@ -35,7 +78,7 @@ define(function (require) {
 					const updateStaffList = (key, value) => ($scope.staffList[key] = value)
 
 					// getting staff List for current change type
-					const res = await pqService.getPQResults('net.cdolinc.staffChanges.staff.changes', pqData)
+					let res = await pqService.getPQResults('net.cdolinc.staffChanges.staff.changes', pqData)
 
 					//updating staffList obj
 					if (res.length > 0) {
@@ -46,6 +89,19 @@ define(function (require) {
 							keys.forEach(key => {
 								item[`${key}_complete`] = item[`${key}_created`] == 1 || item[`${key}_ignored`] == 1
 							})
+
+							Object.keys(item).forEach(key => {
+								if (key.endsWith('_date')) {
+									item[key] = new Date(formatService.formatDateFromApi(item[key]))
+								}
+							})
+
+							if (item.final_completion_date) {
+								console.log(typeof item.final_completion_date)
+								item.completed = true
+							} else {
+								item.completed = false
+							}
 						})
 					} else {
 						$scope.staffList[changeType] = {}
@@ -66,6 +122,7 @@ define(function (require) {
 			$scope.reloadData = () => {
 				$scope.staffChangeCounts = []
 				$scope.staffList = {}
+				// $scope.schoolMap ={}
 				$scope.selectedTab = document.querySelector('[aria-selected="true"]').getAttribute('data-context')
 				$scope.loadData($scope.selectedTab)
 			}
