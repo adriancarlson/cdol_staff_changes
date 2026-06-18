1c07ded1f32c4a371b3a939df51580741225ac2f
Adrian Carlson <adrian.r.carlson@gmail.com>
2026-06-18 16:11:07 -0500

    Bump plugin version to 26.6.2.45, refactor directives to use const, and replace tests with debug panel for improved functionality and maintainability

---
+++ web_root/scripts/components/staff_change/directives/tabs/index.js
@@ -0,0 +1,12 @@
+'use strict'
+define(function (require) {
+	// Register list-tab directives and the export control used by the list page.
+	require('components/staff_change/directives/tabs/all_staff_list')
+	require('components/staff_change/directives/tabs/exit_staff_list')
+	require('components/staff_change/directives/tabs/export_button')
+	require('components/staff_change/directives/tabs/job_change_list')
+	require('components/staff_change/directives/tabs/name_change_list')
+	require('components/staff_change/directives/tabs/new_staff_list')
+	require('components/staff_change/directives/tabs/sub_staff_list')
+	require('components/staff_change/directives/tabs/transfer_staff_list')
+})
